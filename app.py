451dad42776d6059f495dad5b29d10bc5cea797b
pinstripezebra <seelcs12@gmail.com>
2023-10-08 12:22:43 -0700

    Update app.py
    Updating app.py, changing url

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ def update_output_div(value):
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True, host="0.0.0.0", port=8080, use_reloader=False)
+    app.run_server(debug=True)
